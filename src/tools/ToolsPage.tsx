a871f0ff2e23379f9abf7a7682cfe2178bec473b
manayparikh1 <manayparikh1@gmail.com>
2026-08-12 20:29:31 -0400

    ToolsPage.tsx imports completed

---
+++ src/tools/ToolsPage.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useRef, useState } from 'react';
+import { DroneBox } from './DroneBox';
+import { EarTrainer } from './EarTrainer';
+import { ScaleFinder } from './ScaleFinder';
+import { TaalMetronome } from './TaalMetronome';
+import { TheoryReference } from './TheoryReference';
+import { Tuner } from './Tuner';
+import './tools.css';
